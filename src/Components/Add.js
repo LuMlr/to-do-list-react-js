94b5114f69e1297e32d3e92b9094da7fc319ff5f
Sindhura <sindhura.kontham@gmail.com>
2022-12-08 16:37:57 +0100

    Add task

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,24 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
-export default function Add() {
 
-    const tasks = ['Get a coffee', 'Buy x-mas presents'];
-    let textTask
-    const taskTextChange = (event) => {
-        textTask = event.target.value
-        console.log(textTask);
-    };
+export default function Add({ tasks, setTasksArray }) {
+    let inputValue;
+       const onChangeFun = (e) => {
+             inputValue = e.target.value;
+            return inputValue
+          }
+        const onClickFun = (e) => {
+            if(inputValue) {
+            let counter = tasks.length
+            counter = counter+1;
+          // slice will create a copy of the array
+          const stateCopy = tasks.slice();
+          stateCopy.unshift({id: counter,task: inputValue });
+          setTasksArray(stateCopy);
+            }
+            else {
+                alert('Required Field: Please enter some text!!')
+            }
 
-    const addTask = () => {
-        alert(textTask);
-        textTask = "";
-    };
+        }
+      return (
+    <div className="row">
+      <div className="additem col backgroundcolor-col pb-2">
+            <div className="row">
+                <h1>To Do List</h1>
+                </div>
+            <div className="row row-cols-md-auto">
+                <div className="col-12 flex-grow-1">
+                <input onChange={onChangeFun} name="name" type="text" className="form-control" id="addItemInput" placeholder="New Task" />
+                </div>
+                <div className="col-12">
+                <button onClick={onClickFun} type="button" id = "addInputButton" className="addbutton btn buttons float-end">Add</button>
+                </div>
+            </div>
+      </div>
+    </div>
 
-    return (
-        <div> 
-            <h1>To-Do-List</h1>
-            <input type="text" placeholder="Add a new task here" onChange={taskTextChange}></input>
-            <input type="submit" value="Add Task" onClick={addTask}></input>
-        </div>
-    );
-}
+      );
+    }
