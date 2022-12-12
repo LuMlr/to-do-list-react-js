f6bdeaa91852eeab1a179079f1387b9629b9399e
Sindhura <sindhura.kontham@gmail.com>
2022-12-12 13:46:49 +0100

    New changes

--- src/Components/Taskitem.js
+++ src/Components/Taskitem.js
@@ -3,23 +3,17 @@ import { useState, useEffect } from "react";
 export default function Taskitem({ task, tasks, setTasksArray, contentEditable }) {
     
    
-const [isActiveDone, setIsActiveDone] = useState(false);
-const [isActiveEdit, setIsActiveEdit] = useState(false);
-
-let taskIndex = task.id - 1
-console.log(taskIndex);
-
-
-  const onDelete = () => {
-
-    const stateCopy = tasks.slice();
-    console.log(stateCopy)
-    let taskIndex = task.id - 1
-    console.log(taskIndex);
-    stateCopy.splice(taskIndex,1);
-    setTasksArray(stateCopy);
-}
+    const [isActiveDone, setIsActiveDone] = useState(false);
+    const [isActiveEdit, setIsActiveEdit] = useState(false);
+    const [doneButton, setDoneButton] = useState('DONE');
+    const [editButton, setEditText] = useState('EDIT');
   
+    const onDelete = () => {
+      const stateCopy = tasks.slice();
+      let index = stateCopy.findIndex((x) => x.task === task.task);
+      stateCopy.splice(index, 1);
+      setTasksArray(stateCopy);
+    };
 
 
     const onDone = () => {
@@ -28,28 +22,43 @@ console.log(taskIndex);
         if (isActiveDone === false) {task.completed=true}
         else{task.completed=false}
         setTasksArray(stateCopy);
+        if (doneButton === "DONE") {
+            setDoneButton("UNDO");
+          } else {
+            setDoneButton("DONE");
+          }
     }
 
     const onEdit = () => {
-        setIsActiveEdit(current => !current);
-}
-
-    return (
+        setIsActiveEdit((current) => !current);
+        if (editButton === "EDIT") {
+            setEditText("SAVE");
+          } else {
+            setEditText("EDIT");
+          }
+      };
+    
+      return (
         <div className="task">
-            <div className="taskitem"
-                style={{textDecorationLine: isActiveDone ? 'line-through' : 'none'}}
-                contentEditable={isActiveEdit ? true : false}
-                >
-                    {task.task}
-                
-            </div>
-                <div className="buttons">
-                    <button onClick={onDone} className={"buttons btn btn-outline-success"}>DONE</button>
-                    <button onClick={onDelete} className={"buttons btn btn-outline-danger"}>DELETE</button>
-                    <button onClick={onEdit} className={"buttons btn btn-outline-warning"}>EDIT</button>
-                </div>
-            
+          <div
+            className="taskitem"
+            style={{ textDecorationLine: isActiveDone ? "line-through" : "none",
+                     color: isActiveEdit ? 'rgb(210, 231, 125)' : "" }}
+            contentEditable={isActiveEdit ? true : false}
+          >
+            {task.task}
+          </div>
+          <div className="buttons">
+            <button onClick={onDone} className={"buttons btn btn-outline-success"}>
+            {doneButton}
+            </button>
+            <button onClick={onDelete} className={"buttons btn btn-outline-danger"}>
+              DELETE
+            </button>
+            <button onClick={onEdit} className={"buttons btn btn-outline-warning"}>
+            {editButton}
+            </button>
+          </div>
         </div>
-    );
-
-}
+      );
+    }
